f1db935b0c88b1c1dc4487ba9d1475154e644050
Onu Rares-Andrei <contact@rares-andrei.me>
2022-07-27 12:52:56 +0300

    +icons

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
-import { Box, Center, Heading } from '@chakra-ui/react';
+import { Box, Center, Container, Heading, Text } from '@chakra-ui/react';
+
+const cardVariants = {
+	offscreen: {
+		y: 300
+	},
+	onscreen: {
+		y: 0,
+		rotate: 0,
+		transition: {
+			type: 'spring',
+			duration: 1,
+			bounce: 0.2
+		}
+	}
+};
+
+function Content(){
+	<Container maxW="container.lg">
+		<Text>trytfhg</Text>
+	</Container>
+}
+
 
 export default function Home() {
 	return (
 		<Box>
-			<Center>
-			<Heading>
-				Test
-			</Heading>
-			</Center>
+			<Content />
 		</Box>
 	);
 }

d51698e1e813f95eb2a4a753615180a262bc2399
Onu Rares-Andrei <contact@rares-andrei.me>
2022-07-27 13:10:06 +0300

    homescreen animations

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import { Box, Center, Container, Heading, Text } from '@chakra-ui/react';
-
+import { Variants, motion } from 'framer-motion';
 const cardVariants = {
 	offscreen: {
-		y: 300
+		y: 500
 	},
 	onscreen: {
 		y: 0,
@@ -10,22 +10,85 @@ const cardVariants = {
 		transition: {
 			type: 'spring',
 			duration: 1,
-			bounce: 0.2
+			bounce: 0
 		}
 	}
 };
 
-function Content(){
-	<Container maxW="container.lg">
-		<Text>trytfhg</Text>
-	</Container>
-}
-
-
 export default function Home() {
 	return (
 		<Box>
-			<Content />
+				<Container maxW="container.lg">
+						<motion.div
+					className="card-container"
+					initial="offscreen"
+					whileInView="onscreen"
+					viewport={{ once: true, amount: 0 }}
+				>
+					<motion.div className="card" variants={cardVariants}>
+						<Box>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading><Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading><Heading>Tetst</Heading>
+							<Heading>Tetst</Heading><Heading>Tetst</Heading>
+
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+						</Box>
+					</motion.div>
+					
+				</motion.div>
+
+				<motion.div
+					className="card-container"
+					initial="offscreen"
+					whileInView="onscreen"
+					viewport={{ once: true, amount: 0.1 }}
+				>
+					<motion.div className="card" variants={cardVariants}>
+						<Box>
+							<Heading>part2</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading><Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading><Heading>Tetst</Heading>
+							<Heading>Tetst</Heading><Heading>Tetst</Heading>
+
+							<Heading>Tetst</Heading>
+							<Heading>Tetst</Heading>
+						</Box>
+					</motion.div>
+					
+				</motion.div>
+	</Container>
 		</Box>
 	);
 }

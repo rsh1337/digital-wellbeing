50ff77e4a7505a62a37e42f4f2f983dd27573218
Onu Rares-Andrei <contact@rares-andrei.me>
2022-07-28 06:02:24 +0300

    add ipone notification 4 install

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,21 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { SessionProvider } from 'next-auth/react';
 import Layout from '../components/Layout';
-
+import loadable from '@loadable/component';
+const PWAPrompt = loadable(() => import('react-ios-pwa-prompt'));
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 	return (
 		<ChakraProvider>
 			<SessionProvider session={session}>
 			<Layout>
 				<Component {...pageProps} />
+				<PWAPrompt
+					copyTitle="Adauga pe ecranul principal"
+					copyBody="Acest website are functionabilitati ca o aplicatie. Adaug-o pe ecranul principal pentru a te bucura de o experienta mai imersiva!"
+					copyShareButtonLabel="1) Apasa pe butonul de 'share'/'distribuire'"
+					copyAddHomeButtonLabel="2) Apasa pe butonul 'Add to homescreen'/'Adauga pe ecranul principal'"
+					copyClosePrompt="Inchide"
+				/>
 			</Layout>
 			</SessionProvider>
 		</ChakraProvider>
